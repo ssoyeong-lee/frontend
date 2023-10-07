eba3d8c9dd9b60de912fa594cfbf4f3e250fddbc
ssoyeong-lee <gaya1273@naver.com>
2023-10-07 14:29:53 +0900

    Feat: splash의 pong game 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Orbitron } from 'next/font/google'
+
+const orbitron = Orbitron({
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+
+      <main className={`${orbitron.className} w-screen h-screen`}>
+        <Component {...pageProps} />
+      </main>
+
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,14 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// text-white  //items-center 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="content-center font-bold text-8xl text-center tracking-wider text-red-600">
+        PONG<br></br>
+        GAME
+      </div>
+    </div>
+  );
 }
