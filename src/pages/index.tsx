8527b7872c6723c1dc5cd6eb42e532dd6bec1a70
ssoyeong-lee <gaya1273@naver.com>
2023-10-07 15:25:51 +0900

    Fix: 필요없는 FlexBox 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <FlexBox className="w-screen h-screen justify-center">
-      <FlexBox>
+    <FlexBox className="h-screen justify-center">
         <div className="font-bold text-8xl text-center tracking-wider text-white">
           PONG<br></br>
           GAME
         </div>
-      </FlexBox>
     </FlexBox>
 
   );
