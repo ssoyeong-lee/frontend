4e9d81a9926cd4467723fe40778ac0a2a68f73cf
Developerryou <developerryou@gmail.com>
2023-10-07 20:43:14 +0900

    FIX: pr 반영전 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,11 @@
-
 import Layout from "@/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Orbitron } from 'next/font/google'
-
-const orbitron = Orbitron({
-  subsets: ['latin'],
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <main className={`${orbitron.className} w-screen h-screen`}>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import FlexBox from "@/layouts/FlexBox";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <FlexBox className="h-screen justify-center">
-        <div className="font-bold text-8xl text-center tracking-wider text-white">
-          PONG<br></br>
-          GAME
-        </div>
+    <FlexBox className="h-full justify-center">
+      <div className="font-bold text-8xl tracking-wider text-white">
+        PONG
+        <br />
+        GAME
+      </div>
     </FlexBox>
-
   );
 }
