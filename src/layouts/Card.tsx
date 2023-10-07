2381e11f76d24f752b90e934835085b153ad374a
Developerryou <developerryou@gmail.com>
2023-10-07 20:21:58 +0900

    FIX: card 레이아웃 수정

--- src/layouts/Card.tsx
+++ src/layouts/Card.tsx
@@ -2,5 +2,9 @@ interface Props {
   children: React.ReactNode;
 }
 export default function Card({ children }: Props) {
-  return <div className="bg-card hover:drop-shadow-card p-6">{children}</div>;
+  return (
+    <div className="w-full h-full bg-card hover:drop-shadow-card p-12 rounded-lg">
+      {children}
+    </div>
+  );
 }
