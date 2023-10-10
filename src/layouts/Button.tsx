5456a27948a24e9d88dcef1cd6212600b78c4a44
soyeong Lee <gaya1273@naver.com>
2023-10-10 22:50:30 +0900

    Feat: Button에 Link 추가

--- src/layouts/Button.tsx
+++ src/layouts/Button.tsx
@@ -1,18 +1,22 @@
 import FlexBox from "@/layouts/FlexBox";
+import Link from "next/link"
 
 interface Props {
+  href: string;
   className?: string;
   textClassName?: string;
   children: React.ReactNode;
 }
 
-export default function Button({ className, textClassName, children }: Props) {
+export default function Button({href, className, textClassName, children }: Props) {
   return (
-    <FlexBox direction="col" className={`justify-center ${className} 
-      ${className?.includes("bg-") ? "" : "bg-[#3691FC]" }`}>
-      <div className={`w-fit h-fit text-white ${textClassName}`}>
-        {children}
-      </div>
-    </FlexBox>
+    <Link href={href} passHref>
+      <FlexBox direction="col" className={`justify-center ${className} 
+        ${className?.includes("bg-") ? "" : "bg-[#3691FC]" }`}>
+        <div className={`w-fit h-fit text-white ${textClassName}`}>
+          {children}
+        </div>
+      </FlexBox>
+    </Link>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,7 @@ import Button from "@/layouts/Button";
 import FlexBox from "@/layouts/FlexBox";
 import Side from "@/layouts/Side";
 
+
 export default function Login() {
   return (
     <Side>
@@ -11,9 +12,9 @@ export default function Login() {
             <div className="font-bold text-5xl tracking-wider leading-[3.5rem]">Login</div>
             <div className="font-bold text-xl tracking-wider">Use 42 Account</div>
           </FlexBox>
-          <Button className="w-[25rem] h-[3rem]" textClassName="font-bold text-2xl tracking-wider">
-            42 Intra로 로그인
-          </Button>
+            <Button href="https://www.naver.com" className="w-[25rem] h-[3rem]" textClassName="font-bold text-2xl tracking-wider">
+              42 Intra로 로그인
+            </Button>
         </FlexBox>
     </Side>
   );
