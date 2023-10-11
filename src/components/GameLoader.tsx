757036e85366c5e1dbd55f52636b0ada5e097fff
Developerryou <developerryou@gmail.com>
2023-10-12 01:21:26 +0900

    FEAT: 자체 Spinning load 제작

--- src/components/GameLoader.tsx
+++ src/components/GameLoader.tsx
@@ -1,15 +1,10 @@
+import SpinningLoad from "@/components/SpinningLoad";
 import FlexBox from "@/layouts/FlexBox";
-import { MoonLoader } from "react-spinners";
 
 export default function GameLoader({ isFinding }: { isFinding: boolean }) {
   return (
     <FlexBox className="gap-8">
-      <MoonLoader
-        color="#ffffff"
-        loading={isFinding}
-        size={30}
-        speedMultiplier={0.4}
-      />
+      <SpinningLoad />
       <div className="text-xl font-bold">Looking For match...</div>
     </FlexBox>
   );
---
+++ src/components/SpinningLoad.tsx
@@ -0,0 +1,9 @@
+import Icon from "@/layouts/Icon";
+
+export default function SpinningLoad() {
+  return (
+    <div className="animate-spin">
+      <Icon src="/icon/spin.png" alt="spin" />
+    </div>
+  );
+}
