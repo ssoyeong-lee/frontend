3569bb0a6bb0a15a1240149836b8b41b10ef6196
soyeong Lee <gaya1273@naver.com>
2023-10-13 22:55:53 +0900

    Feat: 닉네임 경고 div 태그 생성

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -13,13 +13,19 @@ export default function Register() {
 
   return (
     <SideBox>
-      <FlexBox direction="col" className="items-start gap-9">
+      <FlexBox direction="col" className="items-start gap-8">
         <FlexBox direction="col" className="items-start gap-3 ">
           <div className="font-bold text-5xl tracking-wider leading-[3.5rem]">
             Register
           </div>
         </FlexBox>
-        <TextBox inputNickname={myInput} placeholder="Nickname" className="w-[25rem] h-[3rem] font-bold text-2xl tracking-wider bg-gray-100"/>
+        <FlexBox direction="col" className="items-end gap-1">
+          <TextBox inputNickname={myInput} placeholder="Nickname" className="w-[25rem] h-[3rem] font-bold text-2xl tracking-wider bg-gray-100"/>
+          <div className="h-[20px] w-fit">
+            {nickName.length > 10? <div className="text-sm tracking-wider text-red-cyber">Must be less than 10 characters</div>: ""}
+          </div>
+        </FlexBox>
+
         <Button
           href="https://www.naver.com"
           className="border rounded w-[25rem] h-[3rem] bg-gray-500"
