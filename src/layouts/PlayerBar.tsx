13e2361217a5ad972a230e557b5a7f686cacc302
soyeong Lee <gaya1273@naver.com>
2023-10-18 17:50:36 +0900

    Feat: 키보드 이벤트 부착

--- src/layouts/PlayerBar.tsx
+++ src/layouts/PlayerBar.tsx
@@ -1,11 +1,16 @@
+import { useEffect, useState } from "react";
+
 interface Props{
     who?: "me" | "opponent";
     where?: number;
 }
 
-export default function PlayerBar({who}: Props){
+export default function PlayerBar({who, where}: Props){
+    let barStyle = {
+        left: `${where}px`,
+    };
     return (
-        <div className={`absolute z-99 left-[50px] w-1/4 h-[20px] bg-white
+        <div style={barStyle} className={`absolute z-99 w-1/4 h-[20px] bg-white
             ${who === "me" ? "bottom-0 mb-4 drop-shadow-red" : "top-0 mt-4 drop-shadow-blue"}`}></div>
     );
 }
