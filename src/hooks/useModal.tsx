352300854aa5db30e3a3202033d9363f8327f32a
DeveloperRyou <DeveloperRyou@gmail.com>
2023-10-28 13:21:14 +0900

    FIX: Lint

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 interface ModalTemplateProps {
   content: React.ReactNode;
