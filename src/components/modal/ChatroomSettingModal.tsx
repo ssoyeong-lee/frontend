755e1106cffeb6537e3dbd2627e11bf8f8d482cf
soyeong Lee <gaya1273@naver.com>
2023-11-04 16:38:36 +0900

    Feat: chatrootSettingModal 추가

--- src/components/modal/ChatroomSettingModal.tsx
+++ src/components/modal/ChatroomSettingModal.tsx
@@ -1,4 +1,19 @@
-export default function ChatroomSettinngModal(){
-	
-	return <div>setting</div>;
+import FlexBox from "@/layouts/FlexBox";
+import DefaultInput from "../control/DefaultInput";
+import SquareButton from "../button/SquareButton";
+
+export default function ChatroomSettinngModal() {
+  
+  return (
+    <FlexBox direction="col" className="w-[300px] h-[300px] gap-6 p-6 text-base tracking-wider bg-gray-600">
+      <div className="font-bold">Change Password</div>
+      <FlexBox direction="col" className="w-full h-full gap-3">
+        <DefaultInput placeholder="new" className="bg-gray-700"/>
+        <DefaultInput placeholder="again" className="bg-gray-700"/>
+      </FlexBox>
+      <FlexBox className="w-full justify-end">
+        <SquareButton className="w-[150px]">Ok</SquareButton>
+      </FlexBox>
+    </FlexBox>
+  );
 }
