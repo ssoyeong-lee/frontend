e7411451306e6069558bdc839ceb76e5a998bccf
DeveloperRyou <DeveloperRyou@gmail.com>
2023-11-04 15:46:22 +0900

    FIX: Search Card focus ref 설정

--- src/card/user/SearchCard.tsx
+++ src/card/user/SearchCard.tsx
@@ -1,13 +1,25 @@
 import IconInput from "@/components/control/IconInput";
 import Card from "@/layouts/Card";
+import { useEffect, useRef } from "react";
 
-export default function SearchCard() {
+interface Props {
+  onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  isSearch?: boolean;
+}
+
+export default function SearchCard({ isSearch, onClick }: Props) {
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    console.log(ref);
+    ref.current?.focus();
+  }, []);
   return (
-    <Card>
+    <Card onClick={onClick}>
       <IconInput
         src="/icon/search.png"
         placeholder="type to search"
         color="red"
+        ref={ref}
       />
     </Card>
   );
--- src/components/control/DefaultInput.tsx
+++ src/components/control/DefaultInput.tsx
@@ -1,24 +1,32 @@
+import { RefObject, forwardRef } from "react";
+
 interface Props {
   className?: string;
   name?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
   onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  ref?: RefObject<HTMLInputElement>;
   placeholder?: string;
   size?: "sm" | "lg";
   color?: "white" | "red";
 }
 
-export default function DefaultInput({
-  className,
-  name,
-  value,
-  onChange,
-  onBlur,
-  placeholder,
-  size = "lg",
-  color = "white",
-}: Props) {
+export default forwardRef<HTMLInputElement, Props>(function DefaultInput(
+  {
+    className,
+    name,
+    value,
+    onChange,
+    onFocus,
+    onBlur,
+    placeholder,
+    size = "lg",
+    color = "white",
+  }: Props,
+  ref
+) {
   const padding = size === "lg" ? "p-4" : "px-4 py-2";
   const borderColor =
     color === "white" ? "border-white" : "border-deepred-cyber";
@@ -31,7 +39,9 @@ export default function DefaultInput({
         value={value}
         onChange={onChange}
         onBlur={onBlur}
+        onFocus={onFocus}
+        ref={ref}
       />
     </div>
   );
-}
+});
