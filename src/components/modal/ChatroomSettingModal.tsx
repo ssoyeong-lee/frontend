f235634431153dc05c465262d5b20f3c96912eee
soyeong Lee <gaya1273@naver.com>
2023-11-04 17:21:09 +0900

    Feat: 비밀번호 변경 시, 두 입력값이 일치하지 않으면 경고메시지 추가

--- src/components/modal/ChatroomSettingModal.tsx
+++ src/components/modal/ChatroomSettingModal.tsx
@@ -1,15 +1,43 @@
 import FlexBox from "@/layouts/FlexBox";
 import DefaultInput from "../control/DefaultInput";
 import SquareButton from "../button/SquareButton";
+import { useState } from "react";
 
 export default function ChatroomSettinngModal() {
-  
+  const [pw, setPw] = useState<string>();
+  const [again, setAgain] = useState<string>();
+
+  const pwChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPw(e.target.value);
+  };
+  const againChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAgain(e.target.value);
+  };
+
   return (
-    <FlexBox direction="col" className="w-[300px] h-[300px] gap-6 p-6 text-base tracking-wider bg-gray-600">
+    <FlexBox
+      direction="col"
+      className="w-[300px] h-[324px] gap-6 p-6 text-base tracking-wider bg-gray-600"
+    >
       <div className="font-bold">Change Password</div>
       <FlexBox direction="col" className="w-full h-full gap-3">
-        <DefaultInput placeholder="new" className="bg-gray-700"/>
-        <DefaultInput placeholder="again" className="bg-gray-700"/>
+        <DefaultInput
+          placeholder="new"
+          onChange={pwChange}
+          className="bg-gray-700"
+        />
+        <DefaultInput
+          placeholder="again"
+          onChange={againChange}
+          className="bg-gray-700"
+        />
+        <div className="h-6 w-full">
+          {again && pw !== again ? (
+            <div className="text-red-cyber text-right">wrong password</div>
+          ) : (
+            <></>
+          )}
+        </div>
       </FlexBox>
       <FlexBox className="w-full justify-end">
         <SquareButton className="w-[150px]">Ok</SquareButton>
