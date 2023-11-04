a96c1a7157e1d0c15ac251dbcc4a1840764919e1
soyeong Lee <gaya1273@naver.com>
2023-11-04 16:23:11 +0900

    Feat: blocklistmodal 추가

--- src/card/chat/ChatCard.tsx
+++ src/card/chat/ChatCard.tsx
@@ -1,16 +1,32 @@
 import ChatDisplay from "@/components/chat/ChatDisplay";
 import IconInput from "@/components/control/IconInput";
+import BlockListModal from "@/components/modal/BlockListModal";
+import ChatroomSettinngModal from "@/components/modal/ChatroomSettingModal";
+import { useModal } from "@/hooks/useModal";
 import Card from "@/layouts/Card";
 import Divider from "@/layouts/Divider";
 import FlexBox from "@/layouts/FlexBox";
+import Icon from "@/layouts/Icon";
 
 export default function ChatCard() {
+  const { openModal } = useModal();
+  const blockClick = () => {
+    openModal(<BlockListModal/>);
+  }
+  const settingClick = () => {
+    openModal(<ChatroomSettinngModal/>);
+  }
+  
   return (
     <Card>
       <FlexBox direction="col" className="w-full h-full gap-6">
         <FlexBox className="w-full justify-between">
           <div>Direct message</div>
-          <div>user121</div>
+          <FlexBox className="gap-3">
+            <div>user121</div>
+            <Icon src="/icon/unselectedBlock.png" onClick={blockClick} className="w-6 h-6" alt="name"/>
+            <Icon src="/icon/unselectedSetting.png" onClick={settingClick} className="w-6 h-6" alt="name"/>
+          </FlexBox>
         </FlexBox>
         <Divider />
         <ChatDisplay />
---
+++ src/components/modal/ChatroomSettingModal.tsx
@@ -0,0 +1,4 @@
+export default function ChatroomSettinngModal(){
+	
+	return <div>setting</div>;
+}
