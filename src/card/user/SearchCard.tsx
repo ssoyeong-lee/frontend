d595be9b1fae71bebd05c52132414a05d7e9959e
DeveloperRyou <DeveloperRyou@gmail.com>
2023-11-04 15:57:12 +0900

    FIX: Search Item 분리 이후 템플릿 작성

--- src/card/user/SearchCard.tsx
+++ src/card/user/SearchCard.tsx
@@ -1,5 +1,7 @@
+import ChipButton from "@/components/button/ChipButton";
 import IconInput from "@/components/control/IconInput";
 import Card from "@/layouts/Card";
+import FlexBox from "@/layouts/FlexBox";
 import { useEffect, useRef } from "react";
 
 interface Props {
@@ -7,20 +9,45 @@ interface Props {
   isSearch?: boolean;
 }
 
+function SearchItem() {
+  return (
+    <FlexBox className="w-full px-4 py-2 justify-between">
+      <div>seud</div>
+      <FlexBox className="gap-6">
+        <ChipButton color="green" className="w-[80px]">
+          invite
+        </ChipButton>
+        <ChipButton color="red" className="w-[80px]">
+          ban
+        </ChipButton>
+      </FlexBox>
+    </FlexBox>
+  );
+}
+
 export default function SearchCard({ isSearch, onClick }: Props) {
   const ref = useRef<HTMLInputElement>(null);
   useEffect(() => {
-    console.log(ref);
-    ref.current?.focus();
-  }, []);
+    if (isSearch) ref.current?.focus();
+  }, [isSearch]);
   return (
     <Card onClick={onClick}>
-      <IconInput
-        src="/icon/search.png"
-        placeholder="type to search"
-        color="red"
-        ref={ref}
-      />
+      <FlexBox direction="col" className="gap-6">
+        <IconInput
+          src="/icon/search.png"
+          placeholder="type to search"
+          color="red"
+          ref={ref}
+        />
+        {isSearch && (
+          <>
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+          </>
+        )}
+      </FlexBox>
     </Card>
   );
 }
