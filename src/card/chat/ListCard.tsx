d36a3f3a6022891c11f29acf07b647eaae0c3860
soyeong Lee <gaya1273@naver.com>
2023-11-08 17:43:24 +0900

    Feat: chat 타입 변경 이벤트 추가

--- src/card/chat/ListCard.tsx
+++ src/card/chat/ListCard.tsx
@@ -5,6 +5,7 @@ import ChannelCreateModal from "@/components/modal/ChannelCreateModal";
 import { useModal } from "@/hooks/useModal";
 import Card from "@/layouts/Card";
 import FlexBox from "@/layouts/FlexBox";
+import { useState } from "react";
 
 export default function ListCard() {
   const { openModal } = useModal();
@@ -12,20 +13,46 @@ export default function ListCard() {
     openModal(<ChannelCreateModal />);
   };
 
+  const [type, setType] = useState<string>("user");
+  const clickUser = () => {
+    setType("user");
+  };
+  const clickChannel = () => {
+    setType("channel");
+  };
+
   return (
     <Card>
       <FlexBox direction="col" className="w-full h-full gap-3">
-        <ChatSwitch type="channel" />
-        <FlexBox direction="col" className="h-full w-full gap-3">
-          <FlexBox className="w-full justify-between p-2 hover:bg-gray-600 cursur-pointer">
-            <div className="font-bold">user1</div>
-            <NotificationDot amount={1} />
+        <ChatSwitch
+          type={type}
+          clickUser={clickUser}
+          clickChannel={clickChannel}
+        />
+        {type === "user" ? (
+          <FlexBox direction="col" className="h-full w-full gap-3">
+            <FlexBox className="w-full justify-between p-2 hover:bg-gray-600 cursur-pointer">
+              <div className="font-bold">user1</div>
+              <NotificationDot amount={1} />
+            </FlexBox>
+            <FlexBox className="w-full justify-between p-2 bg-gray-600 cursur-pointer">
+              <div className="font-bold">user2</div>
+              <NotificationDot amount={0} />
+            </FlexBox>
           </FlexBox>
-          <FlexBox className="w-full justify-between p-2 bg-gray-600 cursur-pointer">
-            <div className="font-bold">user2</div>
-            <NotificationDot amount={0} />
+        ) : (
+          <FlexBox direction="col" className="h-full w-full gap-3">
+            <FlexBox className="w-full justify-between p-2 hover:bg-gray-600 cursur-pointer">
+              <div className="font-bold">channel1</div>
+              <NotificationDot amount={1} />
+            </FlexBox>
+            <FlexBox className="w-full justify-between p-2 bg-gray-600 cursur-pointer">
+              <div className="font-bold">channel2</div>
+              <NotificationDot amount={0} />
+            </FlexBox>
           </FlexBox>
-        </FlexBox>
+        )}
+
         <SquareButton onClick={onClick}>Create Channel</SquareButton>
       </FlexBox>
     </Card>
--- src/components/chat/ChatSwitch.tsx
+++ src/components/chat/ChatSwitch.tsx
@@ -1,6 +1,13 @@
 import FlexBox from "@/layouts/FlexBox";
 
-export default function ChatSwitch({ type }: { type: "user" | "channel" }) {
+interface Props {
+  type: string;
+  clickUser: React.MouseEventHandler<HTMLDivElement>;
+  clickChannel: React.MouseEventHandler<HTMLDivElement>;
+}
+
+export default function ChatSwitch({type, clickUser, clickChannel}: Props) {
+
   const selectedStyle = "bg-purple-cyber text-white";
   const unselectedStyle = "text-gray-400 hover:text-white";
   const userStyle = type === "user" ? selectedStyle : unselectedStyle;
@@ -8,11 +15,13 @@ export default function ChatSwitch({ type }: { type: "user" | "channel" }) {
   return (
     <FlexBox className="w-full border-b-2 border-purple-cyber text-center">
       <div
+        onClick={clickUser}
         className={`w-full py-1 font-bold text-xl cursor-pointer ${userStyle}`}
       >
         USER
       </div>
       <div
+        onClick={clickChannel}
         className={`w-full py-1 font-bold text-xl cursor-pointer ${channelStyle}`}
       >
         CHANNEL
