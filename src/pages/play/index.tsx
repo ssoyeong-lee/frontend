9c9b0b03a7e05df6dcc6a669be6e542c47da3209
DeveloperRyou <DeveloperRyou@gmail.com>
2023-11-09 17:57:06 +0900

    FIX: login 리다이렉트 설정

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -1,13 +1,26 @@
 import HowToPlayCard from "@/card/play/HowToPlayCard";
 import OptionCard from "@/card/play/OptionCard";
-import QueueCard from "@/card/play/QueueCard";
 import GameLoader from "@/components/GameLoader";
 import GameButton from "@/components/button/GameButton";
 import Container from "@/layouts/Container";
 import FlexBox from "@/layouts/FlexBox";
 import TopNav from "@/layouts/TopNav";
+import { useState } from "react";
+
+export interface Option {
+  speed: "normal" | "fast";
+  mode: "standard" | "extreme";
+}
 
 export default function Play() {
+  const [isFinding, setIsFinding] = useState(false);
+  const [option, setOption] = useState<Option>({
+    speed: "normal",
+    mode: "standard",
+  });
+  const onClick = () => {
+    setIsFinding((prev) => !prev);
+  };
   return (
     <>
       <TopNav />
@@ -18,13 +31,10 @@ export default function Play() {
           </div>
           <FlexBox className="basis-2/3 h-full gap-6" direction="col">
             <div className="h-fit w-full">
-              <OptionCard />
+              <OptionCard option={option} setOption={setOption} />
             </div>
-            <div className="h-fit w-full">
-              <QueueCard queue={1} game={2} />
-            </div>
-            <GameButton isFinding={false} />
-            <GameLoader isFinding={true} />
+            <GameButton isFinding={isFinding} onClick={onClick} />
+            {isFinding && <GameLoader />}
           </FlexBox>
         </FlexBox>
       </Container>
