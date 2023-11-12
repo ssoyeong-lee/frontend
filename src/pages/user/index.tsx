e622a910fed614f5a486f9af1c5138cc25bf5734
DeveloperRyou <DeveloperRyou@gmail.com>
2023-11-12 16:55:40 +0900

    FIX: userList, friendList, blockList 이용 렌더링

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,34 +1,53 @@
-import BansCard from "@/card/user/BansCard";
-import FriendsCard from "@/card/user/FriendsCard";
+import { Block, getBlockList } from "@/api/users/block";
+import { Friend, getFriendList } from "@/api/users/friend";
+import { UserAbstract, getUserList } from "@/api/users/index";
+import BlockCard from "@/card/user/BlockCard";
+import FriendCard from "@/card/user/FriendCard";
 import RankingCard from "@/card/user/RankingCard";
 import SearchCard from "@/card/user/SearchCard";
 import Container from "@/layouts/Container";
 import FlexBox from "@/layouts/FlexBox";
 import TopNav from "@/layouts/TopNav";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function User() {
+  const [userList, setUserList] = useState<UserAbstract[]>([]);
+  const [friendList, setFriendList] = useState<Friend[]>([]);
+  const [blockList, setBlockList] = useState<Block[]>([]);
   const [isSearch, setIsSearch] = useState(false);
+
+  useEffect(() => {
+    const asyncFunc = async () => {
+      Promise.all([getUserList(), getFriendList(), getBlockList()]).then(
+        (res) => {
+          setUserList(res[0].data);
+          setFriendList(res[1].data);
+          setBlockList(res[2].data);
+        }
+      );
+    };
+    asyncFunc();
+  }, []);
   return (
     <div className="w-full h-full" onClick={() => setIsSearch(false)}>
       <TopNav />
       <Container>
         <div className="w-full h-full" onClick={(e) => e.stopPropagation()}>
           {isSearch ? (
-            <SearchCard isSearch={isSearch} />
+            <SearchCard userList={userList} isSearch={isSearch} />
           ) : (
             <FlexBox className="h-full w-full gap-6">
               <FlexBox className="basis-1/3 h-full gap-6" direction="col">
                 <div className="w-full h-fit">
                   <SearchCard onClick={() => setIsSearch(true)} />
                 </div>
-                <RankingCard />
+                <RankingCard userList={userList} />
               </FlexBox>
               <div className="basis-1/3 h-full">
-                <FriendsCard />
+                <FriendCard friendList={friendList} />
               </div>
               <div className="basis-1/3 h-full">
-                <BansCard />
+                <BlockCard blockList={blockList} />
               </div>
             </FlexBox>
           )}
