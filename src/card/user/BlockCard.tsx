72eaa66423793c8ed922e0dd8fcc7c4140cc6790
DeveloperRyou <DeveloperRyou@gmail.com>
2023-11-12 15:27:54 +0900

    FIX: props 를 이용한 렌더링

--- src/card/user/BlockCard.tsx
+++ src/card/user/BlockCard.tsx
@@ -11,15 +11,19 @@ export default function BlockCard({ blockList }: Props) {
   return (
     <Card>
       <FlexBox className="h-full gap-8 text-xl" direction="col">
-        <div>Bans</div>
-        <FlexBox className="w-full justify-between">
-          <div>hello</div>
-          <Icon
-            src="/icon/delete.png"
-            alt="delete"
-            className="w-[24px] h-[24px]"
-          />
-        </FlexBox>
+        <div>Blocks</div>
+        {blockList.map((block, idx) => {
+          return (
+            <FlexBox className="w-full justify-between" key={idx}>
+              <div>{block.nickname}</div>
+              <Icon
+                src="/icon/delete.png"
+                alt="delete"
+                className="w-[24px] h-[24px]"
+              />
+            </FlexBox>
+          );
+        })}
       </FlexBox>
     </Card>
   );
--- src/card/user/FriendCard.tsx
+++ src/card/user/FriendCard.tsx
@@ -12,29 +12,25 @@ export default function FriendCard({ friendList }: Props) {
     <Card>
       <FlexBox className="h-full gap-8 text-xl" direction="col">
         <div>Friends</div>
-        <FlexBox className="w-full justify-between">
-          <div>hello</div>
-          <Icon
-            src="/icon/delete.png"
-            alt="delete"
-            className="w-[24px] h-[24px]"
-          />
-        </FlexBox>
-        <FlexBox className="w-full justify-between">
-          <div>hello</div>
-          <FlexBox className="gap-4">
-            <Icon
-              src="/icon/check.png"
-              alt="check"
-              className="w-[24px] h-[24px]"
-            />
-            <Icon
-              src="/icon/delete.png"
-              alt="delete"
-              className="w-[24px] h-[24px]"
-            />
-          </FlexBox>
-        </FlexBox>
+        {friendList.map((friend, idx) => {
+          return (
+            <FlexBox className="w-full justify-between" key={idx}>
+              <div>{friend.nickname}</div>
+              <FlexBox className="gap-4">
+                <Icon
+                  src="/icon/check.png"
+                  alt="check"
+                  className="w-[24px] h-[24px]"
+                />
+                <Icon
+                  src="/icon/delete.png"
+                  alt="delete"
+                  className="w-[24px] h-[24px]"
+                />
+              </FlexBox>
+            </FlexBox>
+          );
+        })}
       </FlexBox>
     </Card>
   );
