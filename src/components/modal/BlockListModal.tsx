42936e045e6faf2da95099437c0e248a46634a02
soyeong Lee <gaya1273@naver.com>
2023-11-15 19:42:36 +0900

    Fix: BlockListModal 에 scrollbox 적용

--- src/components/modal/BlockListModal.tsx
+++ src/components/modal/BlockListModal.tsx
@@ -1,4 +1,5 @@
 import FlexBox from "@/layouts/FlexBox";
+import ScrollBox from "@/layouts/ScrollBox";
 import React from "react";
 
 interface Props {
@@ -6,26 +7,49 @@ interface Props {
 }
 
 function UserNickname({ nickname }: Props) {
-  return <div className="w-full text-base tracking-wider font-bold">{nickname}</div>;
+  return (
+    <div className="w-full text-base tracking-wider font-bold">{nickname}</div>
+  );
 }
 
 export default function BlockListModal() {
   return (
-    <FlexBox direction="col" className="w-[300px] h-[300px] gap-6 p-6 bg-gray-600">
+    <FlexBox
+      direction="col"
+      className="w-[300px] h-[300px] gap-6 p-6 bg-gray-600"
+    >
       <div className="w-full border-solid border-b text-base tracking-wider font-bold">
         Blocked User List
       </div>
-      <FlexBox direction="col" className="w-full gap-3 overflow-auto">
-        <UserNickname nickname="user1" />
-        <UserNickname nickname="user2" />
-        <UserNickname nickname="user3" />
-        <UserNickname nickname="user1" />
-        <UserNickname nickname="user2" />
-        <UserNickname nickname="user3" />
-        <UserNickname nickname="user1" />
-        <UserNickname nickname="user2" />
-        <UserNickname nickname="user3" />
-      </FlexBox>
+      <ScrollBox className="gap-6">
+        <FlexBox direction="col" className="w-full gap-3 overflow-y-auto">
+          <UserNickname nickname="user1" />
+          <UserNickname nickname="user2" />
+          <UserNickname nickname="user3" />
+          <UserNickname nickname="user1" />
+          <UserNickname nickname="user2" />
+          <UserNickname nickname="user3" />
+          <UserNickname nickname="user1" />
+          <UserNickname nickname="user2" />
+          <UserNickname nickname="user3" />
+        </FlexBox>
+      </ScrollBox>
     </FlexBox>
+    // <FlexBox direction="col" className="w-[300px] h-[300px] gap-6 p-6 bg-gray-600">
+    //   <div className="w-full border-solid border-b text-base tracking-wider font-bold">
+    //     Blocked User List
+    //   </div>
+    //   <FlexBox direction="col" className="w-full gap-3 overflow-y-auto">
+    //     <UserNickname nickname="user1" />
+    //     <UserNickname nickname="user2" />
+    //     <UserNickname nickname="user3" />
+    //     <UserNickname nickname="user1" />
+    //     <UserNickname nickname="user2" />
+    //     <UserNickname nickname="user3" />
+    //     <UserNickname nickname="user1" />
+    //     <UserNickname nickname="user2" />
+    //     <UserNickname nickname="user3" />
+    //   </FlexBox>
+    // </FlexBox>
   );
 }
