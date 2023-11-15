c58a2b99172efe16573d1e429d874f8ad0150761
soyeong Lee <gaya1273@naver.com>
2023-11-15 19:25:16 +0900

    Fix: context에 폰트 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,11 @@
-import { AuthProvider } from "@/hooks/useAuth";
-import { ModalProvider } from "@/hooks/useModal";
-import { NotificationProvider } from "@/hooks/useNotification";
-import { ToastProvider } from "@/hooks/useToast";
-import { UserControlProvider } from "@/hooks/useUserControl";
 import Layout from "@/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <UserControlProvider>
-        <ModalProvider>
-          <ToastProvider>
-            <NotificationProvider>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-            </NotificationProvider>
-          </ToastProvider>
-        </ModalProvider>
-      </UserControlProvider>
-    </AuthProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
