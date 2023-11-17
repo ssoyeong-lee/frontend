9db503dbd069a6ec2350eb6c8971ad811fb61ef4
soyeong Lee <gaya1273@naver.com>
2023-11-18 00:23:19 +0900

    Refactor: owner에 작성한 api index.ts 로 이동

--- src/api/channels/index.ts
+++ src/api/channels/index.ts
@@ -15,5 +15,20 @@ async function getChannel(channel_id: number){
     return axios.get(`/api/channels/${channel_id}`);
 }
 
+interface Props {
+    title: string;
+    type: string;
+    password?: string | null;
+}
+
+async function createChannel({title, type, password}: Props){
+    return axios.post('/api/channels', {title, type, password});
+}
+
+async function updateChannel(channel_id: number, {title, type, password}: Props){
+    console.log(password);
+    return axios.put(`/api/channels/${channel_id}`, {title, type, password});
+}
+
 export type { Channel };
-export { getChannelList, getChannel };
+export { getChannelList, getChannel, createChannel, updateChannel };
--- src/api/channels/owner.ts
+++ src/api/channels/owner.ts
@@ -1,18 +1,2 @@
 import axios from "axios"
 
-interface Props {
-    title: string;
-    type: string;
-    password?: string | null;
-}
-
-async function createChannel({title, type, password}: Props){
-    return axios.post('/api/channels', {title, type, password});
-}
-
-async function updateChannel(channel_id: number, {title, type, password}: Props){
-    console.log(password);
-    return axios.put(`/api/channels/${channel_id}`, {title, type, password});
-}
-
-export {createChannel, updateChannel};
