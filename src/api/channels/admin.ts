8d2c8c05d6322f81d3f2286aa07fa56b5a90b8b6
soyeong Lee <gaya1273@naver.com>
2023-11-18 01:37:20 +0900

    Fix: admin, owner api의 uri 수정

--- src/api/channels/admin.ts
+++ src/api/channels/admin.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 async function giveAdmin(channel_id: number, user_id: number) {
-  return axios.put(`/channels/${channel_id}/admin/${user_id}/give`);
+  return axios.put(`/api/channels/${channel_id}/admin/${user_id}/give`, {});
 }
 
 async function depriveAdmin(channel_id: number, user_id: number) {
-  return axios.put(`/channels/${channel_id}/admin/${user_id}/deprive`);
+  return axios.put(`/api/channels/${channel_id}/admin/${user_id}/deprive`, {});
 }
 
 export { giveAdmin, depriveAdmin };
--- src/api/channels/owner.ts
+++ src/api/channels/owner.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 async function delegateOwner(channel_id: number, user_id: number) {
-  return axios.put(`/channels/${channel_id}/owner/${user_id}`);
+  return axios.put(`/api/channels/${channel_id}/owner/${user_id}`, {});
 }
 
 export { delegateOwner };
