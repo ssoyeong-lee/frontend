7461346b51d975be5e4e3fc13d9d8f5ad78cc390
soyeong Lee <gaya1273@naver.com>
2023-11-29 01:03:42 +0900

    Feat: 채널 리스트 업데이트 시 조인한 채널 + 그렇지 않은 채널 정렬 기능 추가

--- src/hooks/data/useChatInfo.tsx
+++ src/hooks/data/useChatInfo.tsx
@@ -1,15 +1,21 @@
 import { atom, useAtom } from "jotai";
-import { Channel, getChannelList } from "@/api/channels";
+import { Channel, getChannelList, getMyChannels } from "@/api/channels";
 
-const typeAtom = atom<string>("DM");
+interface ChannelInfoType extends Channel {
+  role: "Owner" | "Admin" | "User" | null;
+}
+
+const typeAtom = atom<"DM" | "CM">("DM");
 const idAtom = atom<number | null>(null);
-const listAtom = atom<Channel[]>([]);
+const listAtom = atom<ChannelInfoType[]>([]);
+const roleAtom = atom<"Owner" | "Admin" | "User" | null>(null);
 
 interface ChatInfoRetType {
   chatInfo: {
-    type: string;
+    type: "DM" | "CM";
     id: number | null;
-    list: Channel[];
+    list: ChannelInfoType[];
+    role: "Owner" | "Admin" | "User" | null;
   };
   changeType: (type: "DM" | "CM") => void;
   changeId: (id: number) => void;
@@ -20,58 +26,46 @@ function useChatInfo(): ChatInfoRetType {
   const [type, setType] = useAtom(typeAtom);
   const [id, setId] = useAtom(idAtom);
   const [list, setList] = useAtom(listAtom);
+  const [role, setRole] = useAtom(roleAtom);
 
-  const changeType = (abc: "DM" | "CM") => {
-    setType(abc);
+  const changeType = (_type: "DM" | "CM") => {
+    setType(_type);
     setId(null);
   };
 
-  const changeId = (id: number) => {
-    setId(id);
+  const changeId = (_id: number) => {
+    setId(_id);
+    if (type == "DM") return;
+    const ret = list.filter((e) => e.id == _id);
+    setRole(ret[0].role);
   };
 
-  const updateList = async (abc: "DM" | "CM") => {
-    if (abc === "DM") {
+  const updateList = async (_type: "DM" | "CM") => {
+    if (_type === "DM") {
       setList([]);
-      console.log(abc, list);
+      console.log(_type, list);
       return;
     }
-    const tmp = await getChannelList();
-    setList(tmp.data);
+    const allChan = (await getChannelList()).data;
+    const myChan = (await getMyChannels()).data;
+
+    const allChanExt = allChan.map((_chan) => {
+      const ret: ChannelInfoType = { ..._chan, role: null };
+      const idx = myChan.findIndex((elem) => elem.channel.id === ret.id);
+      if (idx !== -1) ret.role = myChan[idx].role;
+      return ret;
+    });
+    const myChanExt = myChan.map((_chan) => {
+      const ret: ChannelInfoType = { ..._chan.channel, role: _chan.role };
+      return ret;
+    });
+    const allChanFlt = allChanExt.filter((_chan) => _chan.role === null);
+    setList(myChanExt.concat(allChanFlt));
+    console.log(myChanExt.concat(allChanFlt));
   };
 
-  const chatInfo = { type, id, list };
+  const chatInfo = { type, id, list, role };
   return { chatInfo, changeType, changeId, updateList };
 }
 
 export default useChatInfo;
-
-// const mode = atom("user");
-// const chatId = atom(-1);
-// const chatList = atom<Channel[]>([]);
-
-// interface ExtraChannel extends Channel {
-//   isJoined:boolean
-// }
-// const updateChannelList = atom(
-//   null,
-//   async (get, set)=> {
-//     const allChannel = (await getChannelList()).data;
-//     const myChannel = (await getMyChannel()).data.map(li=>li.channel);
-//     const newAllChannel: ExtraChannel[] = allChannel.map((ch) =>{
-//       const newCh = {...ch, isJoined : false};
-//       if (myChannel.has(ch.id))
-//         newCh.isJoined = true;
-//       else
-//         newCh.isJoined = false;
-//       return newCh;
-//     });
-//     set(chatList, myChannel.concat(allChannel));
-//   }
-// )
-
-// getChannelList.filter((ch) => ch.isJoined === true).map(() => {
-//   <></>
-// })
-
-// export { mode, chatId, chatList, updateChannelList };
