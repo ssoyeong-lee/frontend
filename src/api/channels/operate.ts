856b5ea6102557400bc0d4d728cfc666b2bea4d8
soyeong Lee <gaya1273@naver.com>
2023-12-09 02:57:01 +0900

    Feat: invite 수락, 거절 api 함수 생성

--- src/api/channels/operate.ts
+++ src/api/channels/operate.ts
@@ -5,6 +5,14 @@ async function inviteUser(channel_id: number, user_id: number) {
   return axios.post(`/api/channels/${channel_id}/invite/${user_id}`, {});
 }
 
+async function acceptInvite(channel_id: number) {
+  return axios.post(`/api/channels/${channel_id}/accept-invite`, {});
+}
+
+async function rejectInvite(channel_id: number) {
+  return axios.post(`/api/channels/${channel_id}/refuse-invite`, {});
+}
+
 async function kickMember(channel_id: number, user_id: number) {
   return axios.delete(`/api/channels/${channel_id}/kick/${user_id}`);
 }
@@ -27,6 +35,8 @@ async function muteMember(channel_id: number, user_id: number) {
 
 export {
   inviteUser,
+  acceptInvite,
+  rejectInvite,
   kickMember,
   getBanMemberList,
   banMember,
