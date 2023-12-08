193aa6e3dd5627539e7a99a77802fcc45f5f8303
DeveloperRyou <DeveloperRyou@gmail.com>
2023-12-09 02:32:14 +0900

    FIX: 로그아웃 상태시 로그인페이지로 이동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "@/layouts/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useAuth } from "@/hooks/data/useAuth";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const { auth } = useAuth();
+  useEffect(() => {
+    if (router.pathname !== "/" && router.pathname !== "/login") {
+      if (!auth) {
+        router.push("/login");
+      }
+    }
+  }, [auth, router]); // TODO : test this
   return (
     <Layout>
       <Component {...pageProps} />
