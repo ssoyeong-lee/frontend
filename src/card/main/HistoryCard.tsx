f0f4234de6b19f62a0f885049c7eb182cb47c4f5
DeveloperRyou <DeveloperRyou@gmail.com>
2023-12-12 12:56:52 +0900

    FIX: 최신순 정렬

--- src/card/main/HistoryCard.tsx
+++ src/card/main/HistoryCard.tsx
@@ -5,73 +5,6 @@ import FlexBox from "@/layouts/FlexBox";
 import ScrollBox from "@/layouts/ScrollBox";
 import { dateFormatter } from "@/utils/formatter";
 
-const templateHistory = [
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-  {
-    date: "22-09-2021 21:00:32",
-    opposite: "ryou73",
-    type: "Ladder",
-    score: "5:3",
-    outcome: "Win",
-    ladder: "+30",
-  },
-];
-
 export default function HistoryCard({ user }: { user: UserDetail | null }) {
   return (
     <Card>
@@ -84,35 +17,41 @@ export default function HistoryCard({ user }: { user: UserDetail | null }) {
         </FlexBox>
         <Divider color="white" />
         <ScrollBox className="flex-1">
-          {user?.matchHistorys?.map((history, idx) => (
-            <FlexBox className="w-full justify-between" key={idx}>
-              <div className="w-2/12 text-start">
-                {dateFormatter(history.playedAt)}
-              </div>
-              <div className="w-1/12">
-                {history.userScore + " : " + history.opponentScore}
-              </div>
-              <div
-                className={`w-1/12 ${
-                  history.result === "win"
-                    ? "text-green-cyber"
-                    : "text-red-cyber"
-                }`}
-              >
-                {history.result}
-              </div>
-              <div
-                className={`w-1/12 ${
-                  history.result === "win"
-                    ? "text-green-cyber"
-                    : "text-red-cyber"
-                }`}
-              >
-                {history.result === "win" && "+"}
-                {history.lpChange}
-              </div>
-            </FlexBox>
-          ))}
+          {user?.matchHistorys
+            ?.sort((a, b) => {
+              return (
+                new Date(b.playedAt).getTime() - new Date(a.playedAt).getTime()
+              );
+            })
+            ?.map((history, idx) => (
+              <FlexBox className="w-full justify-between" key={idx}>
+                <div className="w-2/12 text-start">
+                  {dateFormatter(history.playedAt)}
+                </div>
+                <div className="w-1/12">
+                  {history.userScore + " : " + history.opponentScore}
+                </div>
+                <div
+                  className={`w-1/12 ${
+                    history.result === "win"
+                      ? "text-green-cyber"
+                      : "text-red-cyber"
+                  }`}
+                >
+                  {history.result}
+                </div>
+                <div
+                  className={`w-1/12 ${
+                    history.result === "win"
+                      ? "text-green-cyber"
+                      : "text-red-cyber"
+                  }`}
+                >
+                  {history.result === "win" && "+"}
+                  {history.lpChange}
+                </div>
+              </FlexBox>
+            ))}
         </ScrollBox>
       </FlexBox>
     </Card>
