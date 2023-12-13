7c012e4a5861590c53ed7601c5ab02719613e064
soyeong Lee <gaya1273@naver.com>
2023-12-13 10:27:08 +0900

    Fix: selected에 dm, channel 모두 저장할 수 있도록 공통 속성 type 부여 및 join api 부착

--- src/hooks/data/useChatInfo.tsx
+++ src/hooks/data/useChatInfo.tsx
@@ -13,28 +13,32 @@ import { AxiosError } from "axios";
 import { toast } from "react-toastify";
 import { OtherUserAbstract } from "@/api/users";
 
-interface FriendInfoType extends Friend {}
+interface DmInfoType extends OtherUserAbstract {
+  chatType: "DM";
+}
 
 interface ChannelInfoType extends Channel {
+  chatType: "CM";
   role: "Owner" | "Admin" | "User" | null;
 }
 
 const typeAtom = atom<"DM" | "CM">("DM");
-const selectedAtom = atom<OtherUserAbstract | ChannelInfoType | null>(null);
-const friendListAtom = atom<OtherUserAbstract[]>([]);
+const selectedAtom = atom<DmInfoType | ChannelInfoType | null>(null);
+const friendListAtom = atom<DmInfoType[]>([]);
 const channelListAtom = atom<ChannelInfoType[]>([]);
 const memberListAtom = atom<MemberDetail[]>([]);
 
 interface ChatInfoRetType {
   chatInfo: {
     type: "DM" | "CM";
-    selected: OtherUserAbstract | ChannelInfoType | null;
-    friendList: OtherUserAbstract[];
+    selected: DmInfoType | ChannelInfoType | null;
+    friendList: DmInfoType[];
     channelList: ChannelInfoType[];
     memberList: MemberDetail[];
   };
-  changeType: (type: "DM" | "CM") => void;
-  changeSelected: (_id: number | null) => Promise<void>;
+  changeType: (_type: "DM" | "CM") => Promise<void>;
+  changeSelected: (_id: number | null, _type?: "DM" | "CM") => Promise<void>;
+  updateInfo: (_type?: "DM" | "CM") => Promise<void>;
   //memeberList 업데이트
 }
 
@@ -45,46 +49,68 @@ function useChatInfo(): ChatInfoRetType {
   const [channelList, setChannelList] = useAtom(channelListAtom);
   const [memberList, setMemberList] = useAtom(memberListAtom);
 
-  const changeType = (_type: "DM" | "CM") => {
+  const changeType = async (_type: "DM" | "CM") => {
     setType(_type);
-    changeSelected(null);
+    await updateInfo(_type);
   };
 
-  const changeSelected = async (_id: number | null) => {
-    await updateInfo();
-    if (_id === null)
+  const changeSelected = async (
+    _id: number | null,
+    _type: "DM" | "CM" = type
+  ) => {
+    if (_id === null) {
+      setSelected(null);
+      await updateInfo();
       return;
-    if (type === "DM") {
-      const _idx = friendList.findIndex((e) => e.id === _id);
-      _idx === null ? setSelected(null) : setSelected(friendList[_idx]);
-    } else if (type === "CM") {
-      const _idx = channelList.findIndex((e) => e.id === _id);
-      _idx === null ? setSelected(null) : setSelected(channelList[_idx]);
-      
-      const chanData = (await getChannel(_id)).data;
-      setMemberList(chanData.users);
     }
+    if (_type === "DM") {
+      console.log("changeSelected DM");
+      const _idx = friendList.findIndex((e) => e.id === _id);
+      _idx === -1 ? setSelected(null) : setSelected(friendList[_idx]);
+    } else if (_type === "CM") {
+      const _idx = channelList.findIndex((e) => e.id === _id);
+      _idx === -1 ? setSelected(null) : setSelected(channelList[_idx]);
+
+      if (
+        channelList[_idx].type !== "protected" &&
+        channelList[_idx].role === null
+      ) {
+        await joinChannel(_id, "");
+        const chanData = (await getChannel(_id)).data;
+        setMemberList(chanData.users);
+      }
+    }
+    await updateInfo(_type);
   };
 
-  const updateInfo = async () => {
+  const updateInfo = async (_type: "DM" | "CM" = type) => {
     try {
-      if (type === "DM") {
+      if (_type === "DM") {
+        console.log("Update DM");
         const myFriends = (await getFriendList()).data;
-        setFriendList(myFriends);
+        setFriendList(
+          myFriends.map((elem) => {
+            return { chatType: "DM", ...elem };
+          })
+        );
         console.log("myFriends: ", myFriends);
         setSelected(null);
-      } else if (type === "CM") {
+      } else if (_type === "CM") {
         const allChan = (await getChannelList()).data;
         const myChan = (await getMyChannels()).data;
 
         const allChanExt = allChan.map((_chan) => {
-          const ret: ChannelInfoType = { ..._chan, role: null };
+          const ret: ChannelInfoType = { ..._chan, role: null, chatType: "CM" };
           const idx = myChan.findIndex((elem) => elem.id === ret.id);
           if (idx !== -1) ret.role = myChan[idx].role;
           return ret;
         });
         const myChanExt = myChan.map((_chan) => {
-          const ret: ChannelInfoType = { ..._chan, role: _chan.role };
+          const ret: ChannelInfoType = {
+            ..._chan,
+            role: _chan.role,
+            chatType: "CM",
+          };
           return ret;
         });
         const allChanFlt = allChanExt.filter((_chan) => _chan.role === null);
@@ -99,8 +125,8 @@ function useChatInfo(): ChatInfoRetType {
   };
 
   const chatInfo = { type, selected, friendList, channelList, memberList };
-  return { chatInfo, changeType, changeSelected };
+  return { chatInfo, changeType, changeSelected, updateInfo };
 }
 
-export type { ChannelInfoType, FriendInfoType };
+export type { ChannelInfoType };
 export default useChatInfo;
