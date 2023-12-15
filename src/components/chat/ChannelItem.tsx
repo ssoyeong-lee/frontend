f86302d050e63c2eb8bcbe3d26c09e46889a7475
soyeong Lee <gaya1273@naver.com>
2023-12-15 20:25:53 +0900

    Fix: ChannelItem 채널명 한 줄에 출력하도록 수정

--- src/components/chat/ChannelItem.tsx
+++ src/components/chat/ChannelItem.tsx
@@ -66,10 +66,10 @@ export default function ChannelItem({
       } hover:bg-gray-600`}
       onClick={itemClick}
     >
-        <div className={`font-bold ${!isJoined && "text-gray-400"}`}>
+        <div className={`w-fit h-fit font-bold ${!isJoined && "text-gray-400"}`}>
           {data.title}
         </div>
-      <FlexBox className="w-full h-fit justify-end gap-4">
+      <FlexBox className="w-fit h-fit justify-end gap-4">
         <NotificationDot amount={notiCount} />
         {isJoined && <LeaveButton id={data.id} />}
       </FlexBox>
